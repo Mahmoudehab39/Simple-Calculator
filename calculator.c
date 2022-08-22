b0ce75a3037fb763e043e863e603c3a6c5d92586
Mahmoudehab39 <mahmoudehab392000@gmail.com>
2022-08-23 00:27:21 +0200

    feat: write the Code
    Write the code lines to make a simple calculator

---
+++ calculator.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+
+
+int main ()
+{
+    int num1=0,num2=0,add=0,sub=0,mul=0,div=0,x=0;
+    printf("Enter the First Number:");
+    scanf("%d",&num1);
+    printf("Enter the Second Number:");
+    scanf("%d",&num2);
+
+    add=num1+num2;
+    sub=num1+num2;
+    mul=num1*num2;
+    div=num1/num2;
+
+
+    printf("The addition is:%d\n",add);
+    printf("The subtraction is:%d\n",sub);
+    printf("The multiplication is:%d\n",mul);
+    printf("The division is:%d\n",div);
+    return 0;
+}
